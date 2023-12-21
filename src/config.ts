84d44ead335b05d965d3ed291f9ace598c4f4889
tian <istiancz@gmail.com>
2023-12-21 12:03:14 +0800

    update

--- src/config.ts
+++ src/config.ts
@@ -1,7 +1,7 @@
 import type { Site, SocialObjects } from "./types";
 
 export const SITE: Site = {
-  website: "istiancz.xyz", // replace this with your deployed domain
+  website: "https://istiancz.xyz", // replace this with your deployed domain
   author: "Sat Naing",
   desc: "A minimal, responsive and SEO-friendly Astro blog theme.",
   title: "AstroPaper",
